7b507d72d26bdfc3f9eea2844d893e47b77257c3
Rashmeet <rashmeet14201@gmail.com>
2023-06-15 11:47:28 +0530

    First commit

---
+++ login/student.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const studentrouter = express.Router;
+
+studentrouter.post('/login',(req, res)=>[
+
+])
+
+app.listen(3000, ()=>{
+    console.log("server started on port")
+})
---
+++ login/teacher.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const teacherrouter = express.Router;
+
+teacherrouter.post('/login',(req, res)=>{
+
+})
+
+app.listen(3000, ()=>{
+    console.log("server started on port")
+})
